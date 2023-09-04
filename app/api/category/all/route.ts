6d08d71086414fa208abaccdef06ecc76126cb49
Giuliano Mozzillo <giulianomozzillo@live.it>
2023-09-04 23:57:35 +0200

    🐛 Category API route fixed

--- app/api/category/all/route.ts
+++ app/api/category/all/route.ts
@@ -2,7 +2,7 @@ import { getAllCategoriesQuery } from '@/client/queries'
 import { Categories } from '@/types'
 import { NextResponse } from 'next/server'
 
-export const GET = async (res: Response) => {
+export const GET = async () => {
     const categories = await getAllCategoriesQuery()
 
     return NextResponse.json(categories)
